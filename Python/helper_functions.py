4dd159bdbbbed360d685c81bab6841268dfb9104
Alvaro <alvaro.gom.000@gmail.com>
2025-02-20 17:26:11 +0100

    Today's work
    Added Python and R scripts dealing with the enrichment of the KOd codon in the sequences of the DEPs

---
+++ Python/helper_functions.py
@@ -0,0 +1,70 @@
+"""
+Define functions that might be needed in any of the other files of the project.
+"""
+
+
+def reverse_transcribe_nucleotide(nt: str):
+    if nt == "A": return("T")
+    elif nt == "C": return("G")
+    elif nt == "G": return ("C")
+    elif nt == "U": return("A")
+    else:
+        print("Provided letter is not a DNA nucleotide")
+
+
+def transcribe_nucleotide(nt: str):
+    if nt == "A": return("U")
+    elif nt == "C": return("G")
+    elif nt == "G": return ("C")
+    elif nt == "T": return("A")
+    else:
+        print("Provided letter is not a DNA nucleotide")
+
+
+def codon_to_anticodon(codon: str):
+    anticodon = ""
+    for nt in codon:
+        new_nt = transcribe_nucleotide(nt)
+        anticodon += new_nt
+    return(anticodon)
+
+
+def anticodon_to_codon(anticodon: str):
+    codon = ""
+    for nt in anticodon:
+        new_nt = reverse_transcribe_nucleotide(nt)
+        codon += new_nt
+    return(codon)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
