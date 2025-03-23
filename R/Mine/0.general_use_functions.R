0c5b02ec83fbfcbe29d13e74e8e59ab58aeabe1d
alvaro <alvaro.gom.000@gmail.com>
2025-03-23 20:24:41 +0100

    some improvements to EA script

--- R/Mine/0.general_use_functions.R
+++ R/Mine/0.general_use_functions.R
@@ -414,7 +414,37 @@ melt <- function(df) {
 
 
 
-
+################################################################################
+# dplyr::summarize without collapsing
+################################################################################
+#' Take a dataframe, the name of one of its columns and a name for the new output
+#' column that will be produced. Return the same dataframe with an extra column,
+#' containing the number of times the term in that row and in the afore 
+#' mentioned column appears throughout the whole column. Basically what 
+#' dplyr::summarize does, but without actually summarizing or collapsing the 
+#' dataset in any way.
+#' 
+#' @param df Input dataframe
+#' @param column_name A string, with the name of the column containing the
+#' terms we want to count 
+#' @param output_column_name A string, with the desired column name for the new
+#' column in the dataframe
+#' 
+#' @return The input dataframe with a new column, counting the appearances of 
+#' the terms in the specified column
+summarize_mine <- function(df, column_name, output_column_name) {
+  # Perform the count
+  temp <- df %>%
+    group_by(get(column_name)) %>%
+    count()
+  
+  # Match to full dataset
+  colnames(temp) <- c(column_name, output_column_name)
+  df <- left_join(df, temp, by = column_name)
+  
+  # Return output
+  return(df)
+}
 
 
 
